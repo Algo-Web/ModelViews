6b3fcc758c5eb14a0e310033032abb04155fd4a7
c-harris <c.harris@communicationinnovations.com.au>
2017-11-18 10:57:53 +0000

    Apply fixes from StyleCI

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -3,20 +3,21 @@ namespace AlgoWeb\ModelViews\Database;
 
 use Illuminate\Database\Connection as BaseConnection;
 use Illuminate\Database\Eloquent\Model;
+
 class Connection extends BaseConnection
 {
     protected static $classes;
 
-    public static function RegisterTableToProvide(Model $ModelOfTable){
-
+    public static function RegisterTableToProvide(Model $ModelOfTable)
+    {
     }
 
     protected $normalizer;
     /**
      * Run a select statement and return a single result.
      *
-     * @param  string  $query
-     * @param  array   $bindings
+     * @param  string $query
+     * @param  array  $bindings
      * @return mixed
      */
     public function selectOne($query, $bindings = array())
@@ -27,8 +28,8 @@ class Connection extends BaseConnection
     /**
      * Run a select statement against the database.
      *
-     * @param  string  $query
-     * @param  array   $bindings
+     * @param  string                                  $query
+     * @param  array                                   $bindings
      * @return \Stidges\LaravelDbNormalizer\Collection
      */
     public function select($query, $bindings = array())
@@ -37,10 +38,3 @@ class Connection extends BaseConnection
         return $records;
     }
 }
-
-
-
-
-
-
-
